0881dddae9b1c20749d779b73c24e62a7b583a16
ngonzales07 <Admin@N-Gonzales-MacBook-Pro.local>
2020-05-20 14:07:29 -0700

    have phone number return

--- includes/shortcodes/call_number.php
+++ includes/shortcodes/call_number.php
@@ -1,11 +1,10 @@
 <?php
 
+
 function shortcode_call_number() {
-	$text = get_field('business_phone_display', 'options');
-	$tel = get_field('business_phone_url', 'options');
-	
-	if ($text && $tel) : 
-		return '<a href="tel:'.$tel.'">'.$text.'</a>';
-	endif;
+
+		$business_location = get_field( 'business_locations', 'options' )[0];
+		return '<a href="tel:' . $business_location['business_phone_url'] . '">' . $business_location['business_phone_display'] . '</a>';
+
 }
-add_shortcode('call_number', 'shortcode_call_number');
+add_shortcode( 'call_number', 'shortcode_call_number' );
